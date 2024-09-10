362299d38cdb845d1bd40f55d2c1b2612f8edb20
Ian Butterworth <i.r.butterworth@gmail.com>
2024-09-10 10:19:05 -0400

    avoid loading pkg until we have to

--- src/LazyArtifacts.jl
+++ src/LazyArtifacts.jl
@@ -9,7 +9,61 @@ using Artifacts: Artifacts,
 export artifact_exists, artifact_path, artifact_meta, artifact_hash,
        select_downloadable_artifacts, find_artifacts_toml, @artifact_str
 
-# define a function for satisfying lazy Artifact downloads
-using Pkg.Artifacts: ensure_artifact_installed
+using Base.BinaryPlatforms: AbstractPlatform, HostPlatform
+using Base: SHA1
+
+"""
+       ensure_artifact_installed(name::String, artifacts_toml::String;
+                                   platform::AbstractPlatform = HostPlatform(),
+                                   pkg_uuid::Union{Base.UUID,Nothing}=nothing,
+                                   verbose::Bool = false,
+                                   quiet_download::Bool = false,
+                                   io::IO=stderr)
+
+Ensures an artifact is installed, downloading it via the download information stored in
+`artifacts_toml` if necessary.  Throws an error if unable to install.
+"""
+function ensure_artifact_installed(name::String, artifacts_toml::String;
+    platform::AbstractPlatform=HostPlatform(),
+    pkg_uuid::Union{Base.UUID,Nothing}=nothing,
+    verbose::Bool=false,
+    quiet_download::Bool=false,
+    io::IO=stderr)
+    meta = artifact_meta(name, artifacts_toml; pkg_uuid=pkg_uuid, platform=platform)
+    if meta === nothing
+        error("Cannot locate artifact '$(name)' in '$(artifacts_toml)'")
+    end
+
+    return ensure_artifact_installed(name, meta, artifacts_toml;
+        platform, verbose, quiet_download, io)
+end
+
+function ensure_artifact_installed(name::String, meta::Dict, artifacts_toml::String;
+    platform::AbstractPlatform=HostPlatform(),
+    verbose::Bool=false,
+    quiet_download::Bool=false,
+    io::IO=stderr)
+
+    hash = SHA1(meta["git-tree-sha1"])
+    if !artifact_exists(hash)
+        # loading Pkg is a bit slow, so we only do it if we need to
+        # and do it in a subprocess to avoid precompilation complexity
+        return read(`$(Base.julia_cmd()) -E '
+        Pkg = Base.require_stdlib(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"));
+        Pkg.try_artifact_download_sources(
+               $(repr(name)),
+               $(repr(hash)),
+               $(repr(meta)),
+               $(repr(artifacts_toml));
+               $(repr(platform)),
+               $(repr(verbose)),
+               $(repr(quiet_download)),
+               $(repr(io))
+        )'
+        `, String)
+    else
+        return artifact_path(hash)
+    end
+end
 
 end
